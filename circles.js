58057ca766d382e4c88845aec8047aafd6575031
travipross <travisprosser@gmail.com>
2018-02-01 10:59:37 -0400

    Clean up for RTAKS demo

--- circles.js
+++ circles.js
@@ -1,13 +1,15 @@
+// runs once
 function setup() {
-  createCanvas(640, 480);
-  background('red');
+    createCanvas(640, 480);  // set up canvas where all objects are drawn
+    background('red');  // set background colour of canvas
 }
 
+// loops indefinitely
 function draw() {
-  if (mouseIsPressed) {
-    background('red');
-  }
-  fill(255);
-  ellipse(mouseX, mouseY, 60, 60);
-  
+    
+    if (mouseIsPressed) {  // set background color again if mouse button is pressed
+        background('red');
+    }
+    fill(255); // set shape fill color to white
+    ellipse(mouseX, mouseY, 60, 60); // draw an ellipse centered on the mouse position with 60 pixel width and height
 }
